5ae8462cb3ed9514e72004fd1f5682c5a4cb0c45
Max <oscar.torres.m@uni.pe>
2025-02-06 13:06:49 -0500

    clase 4

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,14 +9,20 @@ public class Player : MonoBehaviour
     [SerializeField] private Vector2 inputVector;
     [SerializeField] private float speed;
     
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
     
     void Update()
     {
         GetInput();
+    }
+
+    private void FixedUpdate() 
+    {
         Move();
     }
 
@@ -25,11 +31,11 @@ public class Player : MonoBehaviour
         float xAxis = Input.GetAxisRaw("Horizontal");
         float yAxis = Input.GetAxisRaw("Vertical");
         
-        inputVector = new Vector2(xAxis, yAxis);
+        inputVector = new Vector2(xAxis, 0f);
     }
 
     private void Move()
     {
-        transform.Translate(inputVector * speed * Time.deltaTime);
+        rb.velocity = new Vector2(inputVector.x * speed, rb.velocity.y);
     }
 }
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+
+    [SerializeField] private float jumpForce = 5.0f;
+
+    [SerializeField] private float raycastDistance = 1.0f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private bool tryJumping = false;
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        GetInput();
+        CheckGround();
+        Jump();
+
+        Debug.Log("isGrounded: " + isGrounded + " tryJumping: " + tryJumping);
+    }
+
+    private void FixedUpdate()
+    {
+        // CheckGround();
+        // Jump();
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, Vector2.down * raycastDistance);
+    }
+
+    private void GetInput()
+    {
+        tryJumping = false;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tryJumping = true;
+            return;
+        }
+    }
+
+    private void Jump()
+    {
+        if (!tryJumping) return;
+        if(!isGrounded) return;
+
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastDistance, groundLayer);
+
+        if (hit.transform != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+    }
+
+
+}
